8c563e1a8b13202df9f3049aad17db42244f7ddd
Ranj1thaa <r.17ranjitha@gmail.com>
2026-02-04 18:24:07 +0530

    Fixed project image imports and updated Projects component

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -8,12 +8,6 @@ import f4 from './18668862.png'
 import f5 from './czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjc5MC1udW5ueS0xNS5wbmc.webp'
 import f6 from './gradient-circle-shape-holographic-vibrant-round-icon-multicolor-buttons-can-be-used-in-banner-social-media-web-as-design-element-free-png.webp'
 import f7 from './pngtree-color-fluid-gradient-hand-drawn-cartoon-irregular-shape-png-image_420981.jpg'
-import CiraLogo from './CiraLogo.jpg'
-import logo from './logo.jpg'
-import SsC from './SsC.png'
-import SSQ1 from './SSQ1.png'
-import SsC2 from './SsC2.png'
-import SSq from './SSq.png'
 const assets={
   profileImg,
   faviconImg,
@@ -25,11 +19,5 @@ const assets={
   f5,
   f6,
   f7,
-  CiraLogo,
-  logo,
-  SsC,
-  SSq,
-  SsC2,
-  SSQ1
 }
 export default assets
