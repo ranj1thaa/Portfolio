53bc46deb4cf3573792430c9832a4566d3c836fb
Ranj1thaa <r.17ranjitha@gmail.com>
2026-02-04 18:08:43 +0530

    Fix asset filenames

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import projects from '../utils/project';
+import projects from '../utils/projects';
 import '../styles/projects.css';
 
 const container = {
